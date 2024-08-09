5e19b6b00390921cc9d78d7589be515453663a54
Phương Linh <phuonglinhpham2602@gmail.com>
2024-08-10 00:20:55 +0700

    HCM-9: Implement css & delete old font & update themes mode

--- healthcare-medipro/src/i18n.ts
+++ healthcare-medipro/src/i18n.ts
@@ -1,6 +1,6 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
-import { LOCALES } from './constant';
+import { LOCALES } from './constants';
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
